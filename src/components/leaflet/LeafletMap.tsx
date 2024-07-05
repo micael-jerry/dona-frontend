05102f7a946a090013c101b8247ecf0b0ca8517f
Micael Jerry <hei.jerry.2@gmail.com>
2024-07-05 09:40:40 +0300

    fix: position

--- src/components/leaflet/LeafletMap.tsx
+++ src/components/leaflet/LeafletMap.tsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./LeafletMap.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { LatLngExpression } from "leaflet";
 
 export const LeafletMap: React.FC = () => {
-  const coordonne: LatLngExpression = [-18.9038592, 47.5292364];
+  const [position, setPosition] = useState<LatLngExpression>([
+    -18.9038592, 47.5292364,
+  ]);
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((p: GeolocationPosition) => {
+        setPosition([p.coords.latitude, p.coords.longitude]);
+      });
+    }
+  }, []);
+
   return (
-    <MapContainer center={coordonne} zoom={13} scrollWheelZoom={true}>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={coordonne}>
+      <Marker position={position}>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          {position.toString()}
         </Popup>
       </Marker>
     </MapContainer>
