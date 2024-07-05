f3cbf9972ffd0b3224f871e9b627e65eb4598a48
Micael Jerry <hei.jerry.2@gmail.com>
2024-07-05 15:02:34 +0300

    fix: auth provider

---
+++ src/provider/AuthProvider.tsx
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import React, { createContext, useEffect, useMemo, useState } from 'react';
+import { TokenType } from '../types/types';
+
+export interface AuthContextType {
+	token: TokenType;
+	setTokenValue: (newToken: string) => void;
+}
+
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export interface AuthProviderProps {
+	children: React.ReactNode;
+}
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+	const [token, setToken] = useState<TokenType>(localStorage.getItem('token'));
+
+	const setTokenValue = (newToken: string) => {
+		setToken(newToken);
+	};
+
+	useEffect(() => {
+		if (token) {
+			axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+			localStorage.setItem('token', token);
+		} else {
+			delete axios.defaults.headers.common['Authorization'];
+			localStorage.removeItem('token');
+		}
+	}, [token]);
+
+	const contextValue = useMemo(() => ({ token, setTokenValue }), [token]);
+
+	return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>;
+};
