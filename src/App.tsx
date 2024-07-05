0ab632c431e21a556ee348b3a1bcf2e59723fce7
Micael Jerry <hei.jerry.2@gmail.com>
2024-07-05 10:18:06 +0300

    chore: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { LeafletMap } from "./components/leaflet/LeafletMap";
+import { LatLng } from "leaflet";
 
-function App() {
-  return (
-    <LeafletMap/>
-  );
-}
+const App = () => {
+	const [position, setPosition] = useState<LatLng>(new LatLng(-18.9038592, 47.5292364));
+
+	useEffect(() => {
+		if ("geolocation" in navigator) {
+			navigator.geolocation.getCurrentPosition((p: GeolocationPosition) => {
+				setPosition(new LatLng(p.coords.latitude, p.coords.longitude));
+			});
+		}
+	}, []);
+
+	return <LeafletMap position={position} />;
+};
 
 export default App;
--- src/components/leaflet/LeafletMap.tsx
+++ src/components/leaflet/LeafletMap.tsx
@@ -1,32 +1,21 @@
-import React, { useEffect, useState } from "react";
 import "./LeafletMap.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { LatLngExpression } from "leaflet";
+import { LatLng } from "leaflet";
 
-export const LeafletMap: React.FC = () => {
-  const [position, setPosition] = useState<LatLngExpression>([
-    -18.9038592, 47.5292364,
-  ]);
+export interface LeafletMapProps {
+	position: LatLng;
+}
 
-  useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition((p: GeolocationPosition) => {
-        setPosition([p.coords.latitude, p.coords.longitude]);
-      });
-    }
-  }, []);
-
-  return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>
-          {position.toString()}
-        </Popup>
-      </Marker>
-    </MapContainer>
-  );
+export const LeafletMap: React.FC<LeafletMapProps> = ({ position }) => {
+	return (
+		<MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			<Marker position={position}>
+				<Popup>{`lat: ${position.lat} lng: ${position.lng}`}</Popup>
+			</Marker>
+		</MapContainer>
+	);
 };
