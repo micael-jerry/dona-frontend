f35abcbbae6893b2b14ff62807048b92da4288f0
Micael Jerry <hei.jerry.2@gmail.com>
2024-07-19 09:00:20 +0300

    chore: show report in map

--- src/components/leaflet/LeafletMap.tsx
+++ src/components/leaflet/LeafletMap.tsx
@@ -1,21 +1,28 @@
 import './LeafletMap.css';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import { LatLng } from 'leaflet';
+import { ReportResponse } from '../../types/api.types';
 
 export interface LeafletMapProps {
 	position: LatLng;
+	reports: ReportResponse[];
 }
 
-export const LeafletMap: React.FC<LeafletMapProps> = ({ position }) => {
+export const LeafletMap: React.FC<LeafletMapProps> = ({ position, reports }) => {
 	return (
 		<MapContainer center={position} zoom={13} scrollWheelZoom={true}>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
-			<Marker position={position}>
-				<Popup>{`lat: ${position.lat} lng: ${position.lng}`}</Popup>
-			</Marker>
+			{reports.map((report) => {
+				const latLng = new LatLng(report.location.coordinates[1], report.location.coordinates[0]);
+				return (
+					<Marker position={latLng} key={`${report.location.coordinates.toString()}`}>
+						<Popup>{`lat: ${latLng.lat} lng: ${latLng.lng}`}</Popup>
+					</Marker>
+				);
+			})}
 		</MapContainer>
 	);
 };
--- src/page/dashboard/Dashboard.tsx
+++ src/page/dashboard/Dashboard.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Layout } from '../../components/layout/Layout';
 import { LAYOUT_MENU } from '../../constants/layout.menu';
 import { usePositionStore } from '../../stores/position';
 import { LeafletMap } from '../../components/leaflet/LeafletMap';
+import { ReportResponse } from '../../types/api.types';
+import { ReportsGetAllReports } from '../../clients/api';
+import { Loader } from '../../components/loader/Loader';
 
 export const Dashboard: React.FC = () => {
+	const [reports, setReports] = useState<ReportResponse[]>([]);
 	const { position } = usePositionStore();
+
+	const getReports = async () => {
+		await ReportsGetAllReports()
+			.then((res) => setReports(res.data))
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	useEffect(() => {
+		getReports();
+	}, []);
+
 	return (
 		<Layout title={'Map'} menu={LAYOUT_MENU}>
-			<LeafletMap position={position} />
+			<Loader load={reports.length === 0}>
+				<LeafletMap position={position} reports={reports} />
+			</Loader>
 		</Layout>
 	);
 };
