b08a5a99477f1d0499f97bdcc0113bde395ca33c
Micael Jerry <hei.jerry.2@gmail.com>
2024-07-25 09:44:24 +0300

    fix: use geolocation hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,8 @@
-import { useCallback, useEffect } from 'react';
 import './App.css';
-import { LatLng } from 'leaflet';
 import { AuthProvider } from './provider/AuthProvider';
 import { Routes } from './routes';
-import { usePositionStore } from './stores/position';
 
 const App = () => {
-	const { setPosition } = usePositionStore();
-
-	const getCurrentLocation = useCallback(() => {
-		if ('geolocation' in navigator) {
-			navigator.geolocation.getCurrentPosition(
-				(p: GeolocationPosition) => {
-					setPosition(new LatLng(p.coords.latitude, p.coords.longitude));
-				},
-				(err: GeolocationPositionError) => {
-					console.log(err);
-				},
-				{
-					enableHighAccuracy: true,
-					timeout: 5000,
-					maximumAge: 0,
-				},
-			);
-		}
-	}, [setPosition]);
-
-	useEffect(() => {
-		getCurrentLocation();
-	}, [getCurrentLocation]);
-
 	return (
 		<AuthProvider>
 			<Routes />
--- src/components/Leaflet/LeafletMap.tsx
+++ src/components/Leaflet/LeafletMap.tsx
@@ -6,13 +6,18 @@ import { useState } from 'react';
 import { Box, Typography } from '@mui/material';
 import dayjs from 'dayjs';
 import { ReportBottomNav } from '../report/BottomNav/ReportBottomNav';
-import { usePositionStore } from '../../stores/position';
+import { useGeolocation } from '@uidotdev/usehooks';
+import { Loader } from '../Loader/Loader';
 
 export interface LeafletMapProps {
 	reports: ReportResponse[];
 }
 
 const markerIcons = {
+	USER: new Icon({
+		iconUrl: 'https://img.icons8.com/?size=100&id=492ILERveW8G&format=png&color=000000',
+		iconSize: [35, 35],
+	}),
 	POLICE: new Icon({
 		iconUrl: 'https://img.icons8.com/?size=100&id=23311&format=png&color=000000',
 		iconSize: [35, 35],
@@ -20,7 +25,7 @@ const markerIcons = {
 };
 
 export const LeafletMap: React.FC<LeafletMapProps> = ({ reports }) => {
-	const { position } = usePositionStore();
+	const { latitude, longitude, loading } = useGeolocation({ enableHighAccuracy: true, timeout: 5000, maximumAge: 0 });
 	const [positionClicked, setPositionClicked] = useState<LatLng | null>(null);
 
 	const MapClickHandler = () => {
@@ -33,32 +38,39 @@ export const LeafletMap: React.FC<LeafletMapProps> = ({ reports }) => {
 	};
 
 	return (
-		<Box>
-			<MapContainer center={position} zoom={13} scrollWheelZoom={true}>
-				<TileLayer
-					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-				/>
-				{reports.map((report) => {
-					const latLng = new LatLng(report.location.coordinates[1], report.location.coordinates[0]);
-					return (
-						<Marker position={latLng} key={`${report.location.coordinates.toString()}`} icon={markerIcons[report.type]}>
-							<Popup>
-								<Typography component={'p'}>Hour: {dayjs(report.createdAt).format('HH:mm:ss')}</Typography>
-								<Typography component={'p'}>Description: {report.description}</Typography>
-								<Typography component={'p'}>{`Lat: ${latLng.lat} Lng: ${latLng.lng}`}</Typography>
-							</Popup>
+		<Loader load={loading}>
+			<Box>
+				<MapContainer center={new LatLng(latitude!, longitude!)} zoom={13} scrollWheelZoom={true}>
+					<TileLayer
+						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					/>
+					{latitude && longitude && <Marker position={new LatLng(latitude, longitude)} icon={markerIcons.USER} />}
+					{reports.map((report) => {
+						const latLng = new LatLng(report.location.coordinates[1], report.location.coordinates[0]);
+						return (
+							<Marker
+								position={latLng}
+								key={`${report.location.coordinates.toString()}`}
+								icon={markerIcons[report.type]}
+							>
+								<Popup>
+									<Typography component={'p'}>Hour: {dayjs(report.createdAt).format('HH:mm:ss')}</Typography>
+									<Typography component={'p'}>Description: {report.description}</Typography>
+									<Typography component={'p'}>{`Lat: ${latLng.lat} Lng: ${latLng.lng}`}</Typography>
+								</Popup>
+							</Marker>
+						);
+					})}
+					{positionClicked && (
+						<Marker position={positionClicked}>
+							<Popup>{`lat: ${positionClicked.lat} lng: ${positionClicked.lng}`}</Popup>
 						</Marker>
-					);
-				})}
-				{positionClicked && (
-					<Marker position={positionClicked}>
-						<Popup>{`lat: ${positionClicked.lat} lng: ${positionClicked.lng}`}</Popup>
-					</Marker>
-				)}
-				<MapClickHandler />
-			</MapContainer>
-			<ReportBottomNav positionClicked={positionClicked} />
-		</Box>
+					)}
+					<MapClickHandler />
+				</MapContainer>
+				<ReportBottomNav positionClicked={positionClicked} />
+			</Box>
+		</Loader>
 	);
 };
